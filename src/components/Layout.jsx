1d6cd2912478c97fb5eaf512b6be7f973b3d3fbe
SunsideAI <contact@sunsideai.de>
2025-12-09 13:59:00 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -178,7 +178,7 @@ function Layout({ children }) {
       {/* Main Content */}
       <main className="pt-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          {children}
+          {children || <Outlet />}
         </div>
       </main>
     </div>
